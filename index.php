e91e4d65c51919ce639f934ce8e9ac8ab62a2e29
Sagar-Sah <67217423+Sagar-Sah@users.noreply.github.com>
2020-12-15 13:39:29 +0545

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<form action="new.php" method="post">
+<form action="source.php" method="post">
   <div class="form-group" >
     <label for="Enter the URL">URL</label>
     <input type="url" class="form-control"  placeholder="Enter the URL" name="url">
