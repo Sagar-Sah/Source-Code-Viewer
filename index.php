326b5c182110335854304da314bff10e0bffb882
Sagar-Sah <67217423+Sagar-Sah@users.noreply.github.com>
2020-12-15 13:36:50 +0545

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<form action="new.php" method="post">
+  <div class="form-group" >
+    <label for="Enter the URL">URL</label>
+    <input type="url" class="form-control"  placeholder="Enter the URL" name="url">
+  </div>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
